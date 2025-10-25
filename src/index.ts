60799c130b7868d675790d247da741036bf14708
karanjar <karanjar990@gmail.com>
2025-10-25 15:48:37 +0300

    say hi

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ export function greet(name: string): string {
     return `Hello, ${name}!`;
 }
 
+export const sayHi = (name: string): string => {
+    return `hi, ${name}!`;
+}
+
 // Named export example
 export const VERSION = "1.0.0";
